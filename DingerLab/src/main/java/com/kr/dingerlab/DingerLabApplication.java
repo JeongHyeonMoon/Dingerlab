a48e36b2d038b2e1e4d8f787c5faed0f4b20a0af
Wooseok Song <bboyskip115@gmail.com>
2017-06-18 03:47:32 +0900

    deploy test

--- DingerLab/src/main/java/com/kr/dingerlab/DingerLabApplication.java
+++ DingerLab/src/main/java/com/kr/dingerlab/DingerLabApplication.java
@@ -8,20 +8,32 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @MapperScan(value={"com.kr.dingerlab.mapper"})
-public class DingerLabApplication extends WebMvcConfigurerAdapter {
+public class DingerLabApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DingerLabApplication.class, args);
+		SpringApplicationBuilder applicationBuilder = 
+				new SpringApplicationBuilder(DingerLabApplication.class);
+		
+		SpringApplication application = applicationBuilder.build();
+		application.run(args);
+//		SpringApplication.run(DingerLabApplication.class, args);
 	}
 	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(DingerLabApplication.class);
+	}
+
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
 		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
